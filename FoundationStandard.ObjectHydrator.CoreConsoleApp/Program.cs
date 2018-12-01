86ee6aa36497139ed80034d9ecf3674d2e3e39a7
Ryan Smith <ryans@ocgoodwill.org>
2018-11-30 21:57:42 -0800

    Added .Net Core Console App

---
+++ FoundationStandard.ObjectHydrator.CoreConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FoundationStandard.ObjectHydrator.CoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hydrator = new Hydrator<Customer>();
+            var customers = hydrator.GetList(5);
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
